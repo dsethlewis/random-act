abe3225ac5900ff85b757dad5dbd0ec7581d5a45
Daniel Lewis <dsethlewis@gmail.com>
2021-03-26 17:08:12 -0400

    Added comments to activity.py

--- roll/activity.py
+++ roll/activity.py
@@ -1,11 +1,12 @@
 import random
 
+# define an activity object
 class Activity:
 
     def __init__(self, title, children = [], rank = 1):
-        self.title = title
-        self.children = []
-        self.rank = rank
+        self.title = title # text of activity
+        self.children = [] # sub-categories of activity
+        self.rank = rank # how many times an activity should appear in tree
         if children:
             self.addChild(children)
 
@@ -20,20 +21,25 @@ class Activity:
             for x in a:
                 self.addChild(x)
 
+    # return true if children attribute is not empty
     def hasChild(self):
-        return len(self.children) != 0
+        return bool(self.children)
     
+    # recurse through activity tree
     def choose(self):
         c = random.choice(self.children)
         print(c.title)
         if c.hasChild():
             c.choose()
     
+    # set a new rank
     def changeRank(self, new_rank):
         self.rank = new_rank
         return self
 
+    # how object will appear when printed
     def __repr__(self):
         return "<Activity title:%s children:%s rank:%s>" % (self.title, self.children, self.rank)
 
+# create an alias for the object
 act = Activity
