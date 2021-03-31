a48ad9442e52ba2ee8bf02aa090cd0c5dc01e5b9
Daniel Lewis <dsethlewis@gmail.com>
2021-03-31 15:30:21 -0400

    calculate and display probabilities of each activity

--- roll/activity.py
+++ roll/activity.py
@@ -3,19 +3,26 @@ import random
 # define an activity object
 class Activity:
 
-    def __init__(self, title, children = [], rank = 1):
+    def __init__(self, title, children = [], parent = None, n_siblings = 1, rank = 1):
+        self = self
         self.title = title # text of activity
         self.children = [] # sub-categories of activity
         self.rank = rank # how many times an activity should appear in tree
+        self.parent = parent
+        self.n_siblings = n_siblings
+        self.prob = (rank / n_siblings) * parent.prob if parent else 1
+        self.n_children = 0
         if children:
+            self.n_children = sum([x.rank if type(x) == Activity else 1 for x in children])
             self.addChild(children)
-
 
     def addChild(self, a):
         '''Constructs a hierarchical activity tree.'''
         if type(a) == str:
-            self.children.append(Activity(a))
+            act_a = Activity(a, parent = self, n_siblings = self.n_children)
+            self.children.append(act_a)
         elif type(a) == Activity:
+            a.changeProb(self.n_children, self.prob)
             self.children.extend([a] * a.rank)
         elif type(a) == list:
             for x in a:
@@ -25,17 +32,21 @@ class Activity:
     def hasChild(self):
         return bool(self.children)
     
-    # recurse through activity tree
+    # recurse through activity tree to select next activity
     def choose(self):
         c = random.choice(self.children)
-        print(c.title)
+        print("{} ({})".format(c.title, round(c.prob, 2)))
         if c.hasChild():
             c.choose()
     
-    # set a new rank
+    # set a new rank and return self
     def changeRank(self, new_rank):
         self.rank = new_rank
         return self
+
+    # set a new probability
+    def changeProb(self, n, pprob):
+        self.prob = (self.rank / n) * pprob
 
     # how object will appear when printed
     def __repr__(self):
--- roll/roll2.py
+++ roll/roll2.py
@@ -5,213 +5,221 @@ import rand_task
 
 # construct a nested tree of activities to choose from
 tree = act("Do something", [
-    act("Workout", [
-        act("Aerobic Fitness", [
-            "Jump rope",
-            "Boxing",
-            "DDR",
-            "Stairs",
-            "Burpees",
-            "Mountain climbers",
-            "Go for a short run"
-        ]),
-        act("Muscular Fitness", [
-            "Squats",
-            "Floor press",
-            "Rows",
-            "Overhead press",
-            "Deadlift",
-            act("Accessory", [
-                act("Core", [
-                    "Plank",
-                    "Side plank",
-                    "Superman"
-                    "TRX knee tucks"
-                    "TRX hip press"
+    act("Get things done", [
+        rand_task.task_tree.changeRank(2), # Do a task from TickTick
+        act("Process stuff", [
+            "Personal email",
+            "UNC email",
+            "Desktop inbox",
+            act("Alternate channels", [
+                "YNAB",
+                "Texts",
+                "Discord",
+                "WhatsApp",
+                "OneNote",
+                "FB Messenger",
+                "TickTick inbox",
+                act("Mail", [
+                    "Move mail from tray to desktop inbox",
+                    "Move mail from mailbox to mail tray"
                 ]),
-                act("Upper body", [
-                    act("Biceps", [
-                        "TRX curls",
-                        "Dumbbell curls"
-                    ]),
-                    act("Triceps", [
-                        "TRX tricep extensions",
-                        "Diamond push-ups",
-                        "Skull crushers"
-                    ]),
-                    act("Shoulders", [
-                        "TRX clock press",
-                        "Resistance band pull aparts"])
-                ]),
-            ])
-        ]),
-        act("Flexibility", [
-            "Happy baby",
-            "Child pose",
-            "Roll neck out",
-            "Swing arms around",
-            "High knees",
-            "Butt kicks",
-            act("Gokhale Method", [
-                "Stretchsitting",
-                "Stacksitting",
-                "Stretchlying",
-                "Tallstanding",
-                "Hip-hinging",
-                "Inner corset",
-                "Glidewalking"
-            ])
-        ]),
-        act("Stability", [
-            "Tree pose",
-            "Balance a book on your head",
-            "Tightrope walk",
-            "Flamingo",
-            "Single-leg bodyweight squat",
-            "Single-leg Romanian deadlift"
-        ])
-    ]),
-    act("Clean", [
-        act("Put something away", rank=2),
-        "Clean a small area of surface",
-        "Wash a dish",
-        "Clean a small area of floor",
-        act("Other", [
-            "Empty a bin (or bring bins out/in",
-            "Spray vinegar mixture in shower",
-            "Move laundry along"
-        ])
-    ]),
-    act("Personal care", [
-        act("Hair and nails", [
-            act("Shower", rank=2),
-            "Brush hair",
-            "Add product to hair",
-            "Shave",
-            "Trim body hair",
-            "Cut nails",
-            "Refill conditioner tube"
-        ]),
-        act("Skin", [
-            "Vaseline lips",
-            "Wash face",
-            "Benzoyl peroxide",
-            "Moisturize",
-            "Face toner",
-            "Triamcinolone acetonide",
-            "Fluticasone",
-            "Acne dot",
-            act("Clindamycin", rank=2),
-            "Refill body wash tube"
-        ]),
-        act("Teeth", [
-            "Floss",
-            "Dental pick",
-            "Fluoride rinse",
-            act("Brush teeth", rank=2)
-        ]),
-        act("Other", [
-            "Eye drops",
-            "Deoderant",
-            "Vaseline nose",
-            "Q-Tip ear",
-            "Wipe glasses",
-            "Pick a random Birch Box thing"
-        ]),
-    ]),
-    act("Send a text", [
-        "Family",
-        "Friend"
-    ]),
-    act("Read", [
-        "A Byte of Python",
-        "Crossroads of Twilight",
-        "Chrome Reading List"
-    ]),
-    rand_task.task_tree.changeRank(2),
-    act("Process stuff", [
-        "Personal email",
-        "UNC email",
-        "Desktop inbox",
-        act("Alternate channels", [
-            "YNAB",
-            "Texts",
-            "Discord",
-            "WhatsApp",
-            "OneNote",
-            "FB Messenger",
-            "TickTick inbox",
-            act("Mail", [
-                "Move mail from tray to desktop inbox",
-                "Move mail from mailbox to mail tray"
             ]),
         ]),
-    ]),
-    act("Relax", [
-        act("Be mindful", [
-            "Meditate",
-            "Mindfully eat a piece of chocolate",
-            "Mindful movement",
-            "Pause, breathe, and listen",
-            "Buddha Board"
+        act("Clean", [
+            act("Put something away", rank=2),
+            "Clean a small area of surface",
+            "Wash a dish",
+            "Clean a small area of floor",
+            act("Other", [
+                "Empty a bin (or bring bins out/in",
+                "Spray vinegar mixture in shower",
+                "Move laundry along"
+            ])
         ]),
-        act("Play", [
-            act("Puzzle", [
-                "Spelling Bee",
-                "Jigsaw puzzle",
-                "Edabit",
-                "DOX"
+    ], rank = 3),
+
+    act("Take care of yourself", [
+        act("Workout", [
+            act("Aerobic Fitness", [
+                "Jump rope",
+                "Boxing",
+                "DDR",
+                "Stairs",
+                "Burpees",
+                "Mountain climbers",
+                "Go for a short run"
             ]),
-            "Color",
-            "Play a video game"
+            act("Muscular Fitness", [
+                "Squats",
+                "Floor press",
+                "Rows",
+                "Overhead press",
+                "Deadlift",
+                act("Accessory", [
+                    act("Core", [
+                        "Plank",
+                        "Side plank",
+                        "Superman"
+                        "TRX knee tucks"
+                        "TRX hip press"
+                    ]),
+                    act("Upper body", [
+                        act("Biceps", [
+                            "TRX curls",
+                            "Dumbbell curls"
+                        ]),
+                        act("Triceps", [
+                            "TRX tricep extensions",
+                            "Diamond push-ups",
+                            "Skull crushers"
+                        ]),
+                        act("Shoulders", [
+                            "TRX clock press",
+                            "Resistance band pull aparts"])
+                    ]),
+                ])
+            ]),
+            act("Flexibility", [
+                "Happy baby",
+                "Child pose",
+                "Roll neck out",
+                "Swing arms around",
+                "High knees",
+                "Butt kicks",
+                act("Gokhale Method", [
+                    "Stretchsitting",
+                    "Stacksitting",
+                    "Stretchlying",
+                    "Tallstanding",
+                    "Hip-hinging",
+                    "Inner corset",
+                    "Glidewalking"
+                ])
+            ]),
+            act("Stability", [
+                "Tree pose",
+                "Balance a book on your head",
+                "Tightrope walk",
+                "Flamingo",
+                "Single-leg bodyweight squat",
+                "Single-leg Romanian deadlift"
+            ])
         ]),
-        act("Enjoy", [
-            "Listen to a song",
-            "Read a poem",
-            "Eat something yummy",
-            "Watch a funny video"
+        act("Personal care", [
+            act("Hair and nails", [
+                act("Shower", rank=2),
+                "Brush hair",
+                "Add product to hair",
+                "Shave",
+                "Trim body hair",
+                "Cut nails",
+                "Refill conditioner tube"
+            ]),
+            act("Skin", [
+                "Vaseline lips",
+                "Wash face",
+                "Benzoyl peroxide",
+                "Moisturize",
+                "Face toner",
+                "Triamcinolone acetonide",
+                "Fluticasone",
+                "Acne dot",
+                act("Clindamycin", rank=2),
+                "Refill body wash tube"
+            ]),
+            act("Teeth", [
+                "Floss",
+                "Dental pick",
+                "Fluoride rinse",
+                act("Brush teeth", rank=2)
+            ]),
+            act("Other", [
+                "Eye drops",
+                "Deoderant",
+                "Vaseline nose",
+                "Q-Tip ear",
+                "Wipe glasses",
+                "Pick a random Birch Box thing"
+            ]),
         ]),
-        act("Rest", [
-            "Shake it out",
-            "Do nothing",
-            "Go out for fresh air"
+        act("Relax", [
+            act("Be mindful", [
+                "Meditate",
+                "Mindfully eat a piece of chocolate",
+                "Mindful movement",
+                "Pause, breathe, and listen",
+                "Buddha Board"
+            ]),
+            act("Play", [
+                act("Puzzle", [
+                    "Spelling Bee",
+                    "Jigsaw puzzle",
+                    "Edabit",
+                    "DOX"
+                ]),
+                "Color",
+                "Play a video game"
+            ]),
+            act("Enjoy", [
+                "Listen to a song",
+                "Read a poem",
+                "Eat something yummy",
+                "Watch a funny video"
+            ]),
+            act("Rest", [
+                "Shake it out",
+                "Do nothing",
+                "Go out for fresh air"
+            ])
+        ]),
+        act("Replenish", [
+            "Eat a piece of fruit",
+            "Eat a vegetable",
+            act("Drink water", rank=2)
+        ]),
+        act("Read", [
+            "A Byte of Python",
+            "Crossroads of Twilight",
+            "Chrome Reading List"
         ])
-    ]),
-    act("Replenish", [
-        "Eat a piece of fruit",
-        "Eat a vegetable",
-        act("Drink water", rank=2)
-    ]),
-    act("Do something nice for Emily", [
-        act("Touch", [
-            "Massage",
-            "Hug",
-            "Kiss",
-          	"Hold hand",
-          	"Shoot your shot"
+    ], rank = 2),
+
+    act("Connect with others", [
+        act("Send a text", [
+            "Family",
+            "Friend"
         ]),
-        act("Words of affirmation", [
-            "Mad props",
-          		"Sweet nothing"
-        ]),
-        "Quality time",
-        act("Acts of service", [
-            "Empty trash or recycling in her office",
-            "Declutter something in her office",
-            "Fold some of her laundry",
-            "Clean her side of the bathroom vanity",
-            "Clean her nightstand",
-            "Offer to solve a problem",
-            "Offer to pick up prescriptions",
-            "Offer to make a phone call she's avoiding",
-            "Clean her computer monitor, desk, whiteboard, or window",
-            "Make the bed"
-        ]),
-       	act("Gifts", [
-            "Snack",
-          	"Drink"
+        act("Do something nice for Emily", [
+            act("Touch", [
+                "Massage",
+                "Hug",
+                "Kiss",
+                "Hold hand",
+                "Shoot your shot"
+            ]),
+            act("Words of affirmation", [
+                "Mad props",
+                    "Sweet nothing"
+            ]),
+            "Quality time",
+            act("Acts of service", [
+                "Empty trash or recycling in her office",
+                "Declutter something in her office",
+                "Fold some of her laundry",
+                "Clean her side of the bathroom vanity",
+                "Clean her nightstand",
+                "Offer to solve a problem",
+                "Offer to pick up prescriptions",
+                "Offer to make a phone call she's avoiding",
+                "Clean her computer monitor, desk, whiteboard, or window",
+                "Make the bed"
+            ]),
+            act("Gifts", [
+                "Snack",
+                "Drink"
+            ])
         ])
-    ])
+    ], rank = 1)
 ])
 
 # t2 = open("roll-table.txt")
