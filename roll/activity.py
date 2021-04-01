e799d95715f02eb031e10c8535b1f941634d6832
Daniel Lewis <dsethlewis@gmail.com>
2021-04-01 09:19:53 -0400

    correct probabilities + reformat summary

--- roll/activity.py
+++ roll/activity.py
@@ -4,14 +4,13 @@ import random
 class Activity:
 
     def __init__(self, title, children = [], parent = None, n_siblings = 1, rank = 1):
-        self = self
         self.title = title # text of activity
         self.children = [] # sub-categories of activity
         self.rank = rank # how many times an activity should appear in tree
         self.parent = parent
         self.n_siblings = n_siblings
         self.prob = (rank / n_siblings) * parent.prob if parent else 1
-        self.n_children = 0
+        self.n_children = 1
         if children:
             self.n_children = sum([x.rank if type(x) == Activity else 1 for x in children])
             self.addChild(children)
@@ -22,8 +21,8 @@ class Activity:
             act_a = Activity(a, parent = self, n_siblings = self.n_children)
             self.children.append(act_a)
         elif type(a) == Activity:
-            a.changeProb(self.n_children, self.prob)
-            self.children.extend([a] * a.rank)
+            act_a = Activity(a.title, a.children, self, self.n_children, a.rank)
+            self.children.extend([act_a] * act_a.rank)
         elif type(a) == list:
             for x in a:
                 self.addChild(x)
@@ -35,7 +34,7 @@ class Activity:
     # recurse through activity tree to select next activity
     def choose(self):
         c = random.choice(self.children)
-        print("{} ({})".format(c.title, round(c.prob, 2)))
+        print("{} ({})".format(c.title, round(c.prob, 3)))
         if c.hasChild():
             c.choose()
     
@@ -44,13 +43,9 @@ class Activity:
         self.rank = new_rank
         return self
 
-    # set a new probability
-    def changeProb(self, n, pprob):
-        self.prob = (self.rank / n) * pprob
-
     # how object will appear when printed
     def __repr__(self):
-        return "<Activity title:%s children:%s rank:%s>" % (self.title, self.children, self.rank)
+        return "<Activity title:%s>" % (self.title)
 
 # create an alias for the object
 act = Activity
--- roll/roll_loop.py
+++ roll/roll_loop.py
@@ -1,58 +1,69 @@
+#!/usr/bin/python3
 from activity import act
 from roll2 import tree
 from datetime import datetime
 import math
+from textwrap import dedent
 
+# simple function that creates a list of initial substrings for a word
+# e.g., aliases("word") -> ["w", "wo", "wor", "word"]
 aliases = lambda word : [word[0:x] for x in range(1, len(word)+1)]
 
+# create list of options for next activity command
 next_options = ["next", "continue", "go", "do", "activity"]
 next_aliases = [""]
 for word in next_options:
     next_aliases += aliases(word)
 
+# create list of options for quit command
 quit_options = ["quit", "exit", "leave"]
 quit_aliases = []
 for word in quit_options:
     quit_aliases += aliases(word)
 
+# follow commands from user to proceed through multiple activities
 def activityLoop():
 
-    running = True
-    n = 0
+    # initialize session
 
-    t = datetime.now()
+    running = True # true while session is active
 
+    n = 0 # number of activities initiated
+
+    t0 = datetime.now() # start time
+
+    # check if program started running before noon
     early_start = False
-    if t.hour < 12:
+    if t0.hour < 12:
         early_start = True
 
+    # session loop
     while running:
 
-        
-
+        # ask user for command
         response = input("\nDo an activity or quit?\n").lower()
 
-        print(" ")
-
-        if response in quit_aliases:
+        # respond to user command
+        if response in quit_aliases: # user wants to quit
             running = False
-        elif early_start and datetime.now().hour >= 12:
+        elif early_start and datetime.now().hour >= 12: # user started before noon and it's now after noon
             print("\n\033[32mIf you haven't already, consider eating lunch.\033[0m\n")
             early_start = False
-        elif response in next_aliases:
+        elif response in next_aliases: # user wants to continue with next activity
             tree.choose()
             n += 1
-        else:
+        else: # user did not select a valid command
             print("\033[31mPlease enter a valid command.\033[0m")
 
-    elapsed = (datetime.now() - t)
+    elapsed = (datetime.now() - t0) # timedelta for how much time passed while program was running
 
-    summary = '''\033[1;34mSummary\033[0m
+    # print a summary of the session
+    summary = '''
+    \033[1;34mSummary\033[0m
     Activities: {}
-    Time elapsed: {}
-    '''.format(n, str(elapsed).split('.')[:-1][0])
+    Time elapsed: {}'''.format(n, str(elapsed).split('.')[:-1][0])
+    print(dedent(summary))
 
-    print(summary)
-
+# start a session
 activityLoop()
 
