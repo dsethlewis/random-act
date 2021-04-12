f1a375b1b4862ac5b32c4cf27782af0d509e2aa4
Daniel Lewis <dsethlewis@gmail.com>
2021-04-11 20:03:14 -0400

    activity history updates

--- roll/rand_read.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-
-import activity
-from microsoftgraph.client import Client
-
-client = Client("dsethlewis@gmail.com", "johnjaco8")
-url = client.authorization_url(redirect_uri, scope, state=None)
