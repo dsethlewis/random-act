8a2d4310fb66637e8e58b89247b56f55262e96fd
Daniel Lewis <dsethlewis@gmail.com>
2021-04-22 19:42:29 -0400

    import hotfix

--- roll/pickling.py
+++ roll/pickling.py
@@ -1,10 +1,11 @@
-import pickle
+from pickle import load, dump
+from alias import all_aliases
 
 def continueSession(filename):
     response = input("\nWould you like to continue from last session? (Y/n) ").lower()
     if response in all_aliases["yes"]:
         infile = open(filename, 'rb')
-        old_jar = pickle.load(infile)
+        old_jar = load(infile)
         infile.close()
         print("Session continued.")
         return old_jar
@@ -16,7 +17,7 @@ def saveAndQuit(filename, jar):
     quit_response = input("Would you like to save? (Y/n) ").lower()
     if quit_response in all_aliases["yes"]:
         outfile = open(filename, 'wb')
-        pickle.dump(jar, outfile)
+        dump(jar, outfile)
         outfile.close()
         print("Session saved.")
     elif quit_response not in all_aliases["no"]:
