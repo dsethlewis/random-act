d1ab91914b69fe68ab6557b6dc29c0a359e1c4c7
Daniel Lewis <dsethlewis@gmail.com>
2021-10-19 08:45:48 -0400

    moves modules to subdirectory db

---
+++ roll/db/database.py
@@ -0,0 +1,33 @@
+from sqlalchemy import create_engine
+# from sqlalchemy.orm import Session
+from sqlalchemy_utils import database_exists, create_database
+
+db_path = r"sqlite:///C:\Users\dseth\Documents\rollpy\mydata\roll.db"
+
+engine = create_engine(db_path)
+if not database_exists(engine.url):
+    create_database(engine.url)
+
+# from models import Base, DBActivity
+
+# with Session(engine) as session:
+#     Base.metadata.create_all(engine)
+#     session.commit()
+
+# from treebuilder import tree
+
+# frame = []
+
+# def detree(node):
+#     for c in node.getChildren():
+#         frame.append(DBActivity(id=c.activity.id,
+#                                 title=c.activity.title,
+#                                 parent_id=c.parent.activity.id))
+#         detree(c)
+
+# detree(tree)
+
+# with Session(engine) as session:
+#     for a in frame:
+#         session.add(a)
+#     session.commit()
---
+++ roll/db/models.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship
+
+Base = declarative_base()
+
+class DBActivity(Base):
+    __tablename__ = 'activity'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(140), nullable=False)
+    status = Column(Boolean)
+    parent_id = Column(Integer, ForeignKey('activity.id'))
+    children = relationship("DBActivity")
+
+class PastActivity(Base):
+    __tablename__ = 'past_activity'
+
+    id = Column(Integer, primary_key=True)
+    activity_id = Column(Integer, ForeignKey('activity.id'))
+    timestamp = Column(DateTime)
