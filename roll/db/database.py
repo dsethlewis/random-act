592eeeb03ec3d931496f6ec7436a59554e822181
Daniel Lewis <dsethlewis@gmail.com>
2021-10-20 03:06:38 -0400

    consolidates db creation for new users

--- roll/db/database.py
+++ roll/db/database.py
@@ -1,9 +1,13 @@
 from sqlalchemy import create_engine
-# from sqlalchemy.orm import Session
 from sqlalchemy_utils import database_exists, create_database
 
-db_path = r"sqlite:///C:\Users\dseth\Documents\rollpy\mydata\roll.db"
+import models, helpers
 
+db_path = r"sqlite:///C:\Users\dseth\Documents\rollpy\mydata\roll.db"
 engine = create_engine(db_path)
+
 if not database_exists(engine.url):
     create_database(engine.url)
+    with helpers.new_session() as session:
+        models.Base.metadata.create_all(engine)
+        session.commit()
--- roll/db/tablemaker.py
+++
@@ -1,5 +0,0 @@
-import database, models, helpers
-
-with helpers.new_session() as session:
-    models.Base.metadata.create_all(database.engine)
-    session.commit()
