a117b01ef52b940e03d15a968a41cf201a1addcb
Daniel Lewis <dsethlewis@gmail.com>
2021-10-20 13:09:11 -0400

    fixes db import statement

--- roll/db/database.py
+++ roll/db/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy_utils import database_exists, create_database
 
-import models, helpers
+from db import models, helpers
 
 db_path = r"sqlite:///C:\Users\dseth\Documents\rollpy\mydata\roll.db"
 engine = create_engine(db_path)
